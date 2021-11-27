123f561a2b2aa3966aa7a6f4eb221461d0420997
shuthedoor <fransrafin@gmail.com>
2021-11-27 23:46:16 +0000

    modal menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
 	return (
@@ -23,6 +24,48 @@ function Header() {
 				<a href='/'>Account</a>
 				<CustomMenu />
 			</RightMenu>
+			<BurgerMenu>
+				<CustomClose />
+				<li>
+					<a href='/'>Existing Inventory</a>
+				</li>
+				<li>
+					<a href='/'>Used Inventory</a>
+				</li>
+				<li>
+					<a href='/'>Trade-in</a>
+				</li>
+				<li>
+					<a href='/'>Test Drive</a>
+				</li>
+				<li>
+					<a href='/'>Cybertruck</a>
+				</li>
+				<li>
+					<a href='/'>Roadster</a>
+				</li>
+				<li>
+					<a href='/'>Semi</a>
+				</li>
+				<li>
+					<a href='/'>Charging</a>
+				</li>
+				<li>
+					<a href='/'>Powerwall</a>
+				</li>
+				<li>
+					<a href='/'>Commercial Energy</a>
+				</li>
+				<li>
+					<a href='/'>Utilities</a>
+				</li>
+				<li>
+					<a href='/'>Find Us</a>
+				</li>
+				<li>
+					<a href='/'>Support</a>
+				</li>
+			</BurgerMenu>
 		</Container>
 	);
 }
@@ -35,10 +78,12 @@ const Container = styled.div`
 	display: flex;
 	align-items: center;
 	padding: 0 20px;
+	justify-content: space-between;
 	/* top: 0;
 	left: 0;
 	right: 0; */
 	width: 100%;
+	z-index: 1;
 `;
 const Menu = styled.div`
 	display: flex;
@@ -58,6 +103,10 @@ const Menu = styled.div`
 		background-color: rgba(255, 255, 255, 0.2);
 		border-radius: 10px;
 		transition: 0.5;
+	}
+
+	@media (max-width: 850px) {
+		display: none;
 	}
 `;
 
@@ -82,4 +131,45 @@ const RightMenu = styled.div`
 const CustomMenu = styled(MenuIcon)`
 	margin-left: 12px;
 	cursor: pointer;
+`;
+
+const BurgerMenu = styled.div`
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	background-color: #fff;
+	width: 280px;
+	list-style: none;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	text-align: start;
+
+	li {
+		font-size: 14.5px;
+		padding: 15px 0;
+	}
+
+	a {
+		padding: 8px 10px;
+		color: #0a0a0aa7;
+	}
+
+	a:hover {
+		background: rgba(145, 143, 143, 0.15);
+		border-radius: 8px;
+		transition: 0.5;
+		color: #0a0a0a;
+	}
+`;
+
+const CustomClose = styled(CloseIcon)`
+	align-self: flex-end;
+	cursor: pointer;
+	color: #0a0a0aa7;
+
+	:hover {
+		color: #0a0a0a;
+	}
 `;
